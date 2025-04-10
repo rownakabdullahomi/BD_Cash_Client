9c567cd1adc7a69cb8a2dac9184dc613e701ebf7
Rownak Abdullah <rownakabdullahomi@gmail.com>
2025-04-11 01:06:56 +0600

    initial project setup done with typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+     <div className='text-red-500 font-bold text text-center font-Gilda'>
+          Test
+     </div>
+    </>
+  )
+}
+
+export default App
