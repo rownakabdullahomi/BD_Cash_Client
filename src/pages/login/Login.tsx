9c469794bfed9bf44b10dc88768ec03e6bb91614
Rownak Abdullah <rownakabdullahomi@gmail.com>
2025-04-12 02:05:19 +0600

    firebase auth email password and google done

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,46 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import SocialLogin from "../../shared/SocialLogin";
 import { FaHome } from "react-icons/fa";
+import { useAuthContext } from "../../providers/AuthProvider";
+import toast from "react-hot-toast";
+import { FormEvent } from "react";
 
 const Login = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const { userLogin, setUser, googleLogin } = useAuthContext();
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    userLogin(email, password)
+      .then((res) => {
+        const user = res.user;
+        setUser(user);
+        toast.success("Login successful!");
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error: Error) => {
+        toast.error("Login failed!  " + error.message);
+      });
+  };
+
+  const handleGoogleLogin = () => {
+    googleLogin()
+      .then((res) => {
+        const user = res.user;
+        setUser(user);
+        toast.success("Google login successful!");
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error) => {
+        toast.error("Google login failed! " + error.message);
+      });
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse w-full lg:gap-20">
@@ -19,28 +56,45 @@ const Login = () => {
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <fieldset className="fieldset">
-              <label className="fieldset-label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
-              <label className="fieldset-label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              {/* <div>
+            <form onSubmit={handleLogin}>
+              <fieldset className="fieldset">
+                <label className="fieldset-label">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="input"
+                  placeholder="Email"
+                />
+                <label className="fieldset-label">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                {/* <div>
                 <a className="link link-hover">Forgot password?</a>
               </div> */}
-              <div className="max-w-[320px]">
-                <button className="btn btn-primary btn-outline mt-4 w-full">
-                  Login
-                </button>
-                <div className="divider text-sm text-gray-500">OR</div>
-                <SocialLogin />
-              </div>
-              <p className="text-center text-sm text-gray-600">
-                Don’t have an account?{" "}
-                <Link to="/signup" className="text-green-600 font-medium">
-                  Register here
-                </Link>
-              </p>
-            </fieldset>
+                <div className="max-w-[320px]">
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-outline mt-4 w-full"
+                  >
+                    Login
+                  </button>
+                  <div className="divider text-sm text-gray-500">OR</div>
+                  <div onClick={handleGoogleLogin}>
+                    <SocialLogin />
+                  </div>
+                </div>
+                <p className="text-center text-sm text-gray-600">
+                  Don’t have an account?{" "}
+                  <Link to="/signup" className="text-green-600 font-medium">
+                    Register here
+                  </Link>
+                </p>
+              </fieldset>
+            </form>
           </div>
         </div>
       </div>
